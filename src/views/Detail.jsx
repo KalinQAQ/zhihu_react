59512b06c3bfd829331e243d7e0e6984b88be243
Kalin <1026486353@qq.com>
2023-12-10 00:54:42 +0800

    ✨ feat(Detail): 详情页数据绑定

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Detail.less";
 import {
   LeftOutline,
@@ -8,13 +8,82 @@ import {
   MoreOutline,
 } from "antd-mobile-icons";
 import { Badge } from "antd-mobile";
+import api from "../api";
+import SkeletonAgain from "../components/SkeletonAgain";
+import { flushSync } from "react-dom";
 
 const Detail = function Detail(props) {
-  let { navigate } = props;
+  let { navigate, params } = props;
+  /* 定义状态 */
+  let [info, setInfo] = useState(null),
+    [extra, setExtra] = useState(null);
+
+  /* 第一次渲染完毕:获取数据  */
+  let link;
+  const handleStyle = (result) => {
+    let { css } = result;
+    if (!Array.isArray(css)) return;
+    css = css[0];
+    if (!css) return;
+    // 创建<LINK>导入样式
+    link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = css;
+    document.head.appendChild(link);
+  };
+  const handleImage = (result) => {
+    let imgPlaceHolder = document.querySelector(".img-place-holder");
+    if (!imgPlaceHolder) return;
+    // 创建大图
+    let tempImg = new Image();
+    tempImg.src = result.image;
+    tempImg.onload = () => {
+      imgPlaceHolder.appendChild(tempImg);
+    };
+    tempImg.onerror = () => {
+      let parent = imgPlaceHolder.parentNode;
+      parent.parentNode.removeChild(parent);
+    };
+  };
+  useState(() => {
+    (async () => {
+      try {
+        let result = await api.queryNewsInfo(params.id);
+        // 处理样式 & 图片
+        flushSync(() => {
+          setInfo(result);
+          handleStyle(result);
+        });
+        handleImage(result);
+      } catch (_) {}
+    })();
+    // 销毁组件：溢出创建的样式
+    return () => {
+      if (link) document.head.removeChild(link);
+    };
+  }, []);
+  useState(() => {
+    (async () => {
+      try {
+        let result = await api.queryStoryExtra(params.id);
+        setExtra(result);
+      } catch (_) {}
+    })();
+  }, []);
+
   return (
     <div className="detail-box">
       {/* 新闻内容 */}
-      <div className="content"></div>
+      {!info ? (
+        <SkeletonAgain />
+      ) : (
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{
+            __html: info.body,
+          }}
+        ></div>
+      )}
 
       {/* 底部图片 */}
       <div className="tab-bar">
@@ -27,10 +96,10 @@ const Detail = function Detail(props) {
           <LeftOutline />
         </div>
         <div className="icons">
-          <Badge content="128">
+          <Badge content={extra ? extra.comments : 0}>
             <MessageOutline />
           </Badge>
-          <Badge content="29">
+          <Badge content={extra ? extra.popularity : 0}>
             <LikeOutline />
           </Badge>
           <span className="stored">
