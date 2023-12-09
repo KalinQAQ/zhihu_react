25d8e30731773909e7b030ff519e135d1d025e36
Kalin <1026486353@qq.com>
2023-12-09 15:32:13 +0800

    ✨ feat(详情页Detail): 详情页结构

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./Detail.less";
+import {
+  LeftOutline,
+  MessageOutline,
+  LikeOutline,
+  StarOutline,
+  MoreOutline,
+} from "antd-mobile-icons";
+import { Badge } from "antd-mobile";
 
 const Detail = function Detail() {
-  return <div className="detail-box">详情页</div>;
+  return (
+    <div className="detail-box">
+      {/* 新闻内容 */}
+      <div className="content"></div>
+
+      {/* 底部图片 */}
+      <div className="tab-bar">
+        <div className="back">
+          <LeftOutline />
+        </div>
+        <div className="icons">
+          <Badge content="128">
+            <MessageOutline />
+          </Badge>
+          <Badge content="29">
+            <LikeOutline />
+          </Badge>
+          <span>
+            <StarOutline />
+          </span>
+          <span>
+            <MoreOutline />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
